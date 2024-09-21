1dd1ff7490ae65abb87156f46e90628a7f01d437
Danilo Rodrigues <zidalo@gmail.com>
2024-09-21 09:43:53 -0300

    feat(home): :sparkles: Load companies on home page

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:tractian_flutter_test/src/data/fake_api.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(
+    ProviderScope(
+      child: const MainApp(),
+    ),
+  );
 }
 
-class MainApp extends StatelessWidget {
+class MainApp extends ConsumerWidget {
   const MainApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
+  Widget build(BuildContext context, WidgetRef ref) {
+    final companies = ref.watch(companiesProvider);
+    return MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+        appBar: AppBar(
+          title: Text('Tractian'),
+          backgroundColor: Colors.blueAccent,
+        ),
+        body: companies.when(
+          data: (companies) => ListView.builder(
+            itemCount: companies.length,
+            itemBuilder: (context, index) => ListTile(
+              title: Text(companies[index].name),
+              subtitle: Text(companies[index].id),
+              onTap: () => print('tap ${companies[index].toString()}'),
+            ),
+          ),
+          error: (error, stackTrace) => Text(error.toString()),
+          loading: () => Text('Loading...'),
         ),
       ),
     );
